aa69e797433ff5d17a0ac27e6eca5f5008453350
misnina <nina.buie@yahoo.com>
2015-12-16 12:48:26 -0600

    Once More With Style

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+     public float dampTime = 0.3f;
+     private Vector3 velocity = Vector3.zero;
+     public Transform target;
+ 
+     void Update () 
+     {
+         if (target.position.x > 0)
+         {
+             Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
+             Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.12f, point.z));
+             Vector3 destination = transform.position + delta;
+             transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+         }
+     
+
+     if (target.position.y > 0)
+         {
+             Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
+             Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.12f, point.z));
+             Vector3 destination = transform.position + delta;
+             transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+         }
+     }
+	
+}
