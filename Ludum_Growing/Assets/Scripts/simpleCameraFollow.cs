e7c7ed8e80cf463db0eb293a8e6f2cd351fa9aab
misnina <nina.buie@yahoo.com>
2015-12-16 12:43:53 -0600

    DO IT

---
+++ Ludum_Growing/Assets/Scripts/simpleCameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class simpleCameraFollow : MonoBehaviour {
+
+	     public Transform target;
+ 
+     void Update ()
+     {
+         Vector3 position = transform.position;
+         position.x = target.transform.position.x;
+         position.y = 3f;
+         transform.position =  position;
+     }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+}
